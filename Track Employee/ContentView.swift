4416a2c0951cc0c268f274d89a2400a8d648759d
Nazmul Alam Nayeem <nayeemhust@gmail.com>
2024-08-16 19:27:50 -0400

    Initial Commit

---
+++ Track Employee/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Track Employee
+//
+//  Created by Nazmul Alam Nayeem on 2024-08-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Track Employee/Track_EmployeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Track_EmployeeApp.swift
+//  Track Employee
+//
+//  Created by Nazmul Alam Nayeem on 2024-08-16.
+//
+
+import SwiftUI
+
+@main
+struct Track_EmployeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
